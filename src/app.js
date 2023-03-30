a962156ca6668bd98ca49c3023fa8d527eb90536
VladFominov <111357638+VladFominov@users.noreply.github.com>
2023-03-30 19:03:33 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const { responseError } = require('./helpers/apiHelpers');
-const { RouteNotFoundError } = require('./helpers/errors');
-const { errorMiddleware } = require('./middlewares/errors');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const { responseError } = require("./helpers/apiHelpers");
+const { RouteNotFoundError } = require("./helpers/errors");
+const { errorMiddleware } = require("./middlewares/errors");
 
 const app = express();
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+// my rout
 
 // add you routes here
 
