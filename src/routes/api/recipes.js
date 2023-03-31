1d50db0ed56160554fd7689df415b01bd6cd5110
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-31 15:44:25 +0300

    pecipes

--- src/routes/api/recipes.js
+++ src/routes/api/recipes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const  controller  = require('../../controllers/recipes');
 const { user: auth } = require('../../middlewares');
-const recipeRouter = express.Router();
+const router = express.Router();
 
-recipeRouter.get('/', auth, controller.getCategories);
+router.get('/', auth, controller.getCategories);
 
-recipeRouter.get('/category/list', auth, controller.getCategories);
+router.get('/category/list', auth, controller.getCategories);
 
-recipeRouter.get('/main-page', auth, controller.getMainPage);
+router.get('/main-page', auth, controller.getMainPage);
 
-recipeRouter.get('/:category', auth, controller.getRecipesByCategory);
+router.get('/:category', auth, controller.getRecipesByCategory);
 
-recipeRouter.get('/:id', auth, controller.getRecipeById);
+router.get('/:id', auth, controller.getRecipeById);
 
-module.exports = recipeRouter;
+module.exports = router;
