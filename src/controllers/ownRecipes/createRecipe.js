875fd7e39741c66d36c46e7bccc5dcfac9ac740e
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-03 13:20:37 +0300

    Refactoring

--- src/controllers/ownRecipes/createRecipe.js
+++ src/controllers/ownRecipes/createRecipe.js
@@ -1,8 +1,8 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require('cloudinary');
 
-const { ValidationError } = require("../../helpers/errors");
-const { asyncWrapper, responseData } = require("../../helpers/apiHelpers");
-const { ownRecipes: service } = require("../../services");
+const { ValidationError } = require('../../helpers/errors');
+const { asyncWrapper, responseData } = require('../../helpers/apiHelpers');
+const { ownRecipes: service } = require('../../services');
 
 const createRecipe = async (req, res) => {
   const { id: owner } = req.user;
@@ -17,10 +17,6 @@ const createRecipe = async (req, res) => {
   try {
     const recipe = await service.create(data);
 
-    if (!recipe) {
-      return new Error();
-    }
-
     res.status(201).json(
       responseData(
         {
@@ -30,8 +26,8 @@ const createRecipe = async (req, res) => {
       )
     );
   } catch (error) {
-    await cloudinary.v2.uploader.destroy(req.file.filename, "image");
-    throw new ValidationError("Validation error");
+    await cloudinary.v2.uploader.destroy(req.file.filename, 'image');
+    throw new ValidationError('Validation error');
   }
 };
 
--- src/controllers/ownRecipes/deleteRecipe.js
+++ src/controllers/ownRecipes/deleteRecipe.js
@@ -1,18 +1,19 @@
-const { asyncWrapper } = require("../../helpers/apiHelpers");
-const { NotFoundError } = require("../../helpers/errors");
-const { ownRecipes: service } = require("../../services");
+const { asyncWrapper } = require('../../helpers/apiHelpers');
+const { NotFoundError } = require('../../helpers/errors');
+const { ownRecipes: service } = require('../../services');
 
 const deleteRecipe = async (req, res) => {
   const { id: owner } = req.user;
   const { recipeId } = req.params;
 
   const result = await service.deleteById(recipeId, owner);
+
   if (!result) {
-    throw new NotFoundError("Recipe with this id not found");
+    throw new NotFoundError('Recipe with this id not found');
   }
 
   res.json({
-    message: "Recipe deleted",
+    message: 'Recipe deleted',
   });
 };
 
