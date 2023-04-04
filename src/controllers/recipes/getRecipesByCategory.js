74affc3468b173740dec5b74ab380ba101ba31ec
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-04-04 10:23:49 +0300

    .

--- src/controllers/recipes/getRecipesByCategory.js
+++ src/controllers/recipes/getRecipesByCategory.js
@@ -4,7 +4,6 @@ const {NotFoundError} = require('../../helpers/errors')
 
 const getRecipesByCategory = async (req, res) => {
   const { category } = req.params;
-  console.log(category);
   const { page = 1, limit = 8 } = req.query;
   const skip = (page - 1) * limit;
 
