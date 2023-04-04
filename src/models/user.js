8a2c60f8e7851a4ea9446b7822a99c7471b4af68
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-04 22:02:32 +0300

    Fix

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const { isEmailValid } = require("../helpers/validation");
+const { isEmailValid } = require('../helpers/validation');
 
 const schema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Name is required"],
+      required: [true, 'Name is required'],
     },
     email: {
       type: String,
-      validate: [isEmailValid, "Please fill a valid email address"],
-      required: [true, "Email is required"],
+      validate: [isEmailValid, 'Please fill a valid email address'],
+      required: [true, 'Email is required'],
       unique: true,
     },
     password: {
       type: String,
       minlength: 3,
       maxlength: 30,
-      required: [true, "Set password for user"],
+      required: [true, 'Set password for user'],
     },
     avatarURL: {
       type: String,
@@ -40,16 +40,13 @@ const schema = new mongoose.Schema(
     },
     verificationToken: {
       type: String,
-      required: [true, "Verify token is required"],
-    },
-    faforites: {
-      type: Array,
+      required: [true, 'Verify token is required'],
     },
   },
   { versionKey: false, timestamps: true }
 );
 
-schema.pre("save", async function () {
+schema.pre('save', async function () {
   if (this.isNew) {
     this.password = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
   }
@@ -59,6 +56,6 @@ schema.methods.validPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const UserModel = mongoose.model("user", schema);
+const UserModel = mongoose.model('user', schema);
 
 module.exports = UserModel;
