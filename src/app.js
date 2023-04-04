e3b19b0e45b26fdeff3db2022af058d3ed9b4809
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-04 21:45:41 +0300

    Swagger

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use('/api/ingredients', routes.ingredients);
 app.use('/api/users', routes.users);
 app.use('/api/shopping-list', routes.shoppingListRouter);
 app.use('/api/recipes', routes.recipes);
-app.use('/api/favorites', routes.favorites);
+app.use('/api/favorite', routes.favorites);
 //==========================
 
 app.use((_, res) => {
--- src/services/favorites.js
+++ src/services/favorites.js
@@ -5,19 +5,28 @@ const getFavoritesRecipes = async (userId) => {
     favorites: {
       $in: [userId],
     },
-  });
+  }).select({ title: 1, category: 1, description: 1, thumb: 1, preview: 1 });
 
   return recipes;
 };
 
 const addRecipeToFavorite = async (recipeId, userId) => {
-  const recipe = await Recipe.findOneAndUpdate({ _id: recipeId }, { $push: { favorites: userId } }, { new: true });
+  const recipe = await Recipe.findOneAndUpdate(
+    { _id: recipeId },
+    { $push: { favorites: userId } },
+    { new: true }
+  ).select({ title: 1, category: 1, description: 1, thumb: 1, preview: 1 });
+  // const recipe = await Recipe.findOneAndUpdate({ _id: recipeId }, { $addToSet: { favorites: userId } }, { new: true });
 
   return recipe;
 };
 
 const deleteRecipeFromFavorites = async (recipeId, userId) => {
-  const recipe = await Recipe.findOneAndUpdate({ _id: recipeId }, { $pull: { favorites: userId } }, { new: true });
+  const recipe = await Recipe.findOneAndUpdate(
+    { _id: recipeId },
+    { $pull: { favorites: userId } },
+    { new: true }
+  ).select({ title: 1, category: 1, description: 1, thumb: 1, preview: 1 });
 
   return recipe;
 };
