6afc842a5b723387d02186948d929bd85ab48271
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-05 10:27:58 +0300

    Fix

--- src/controllers/auth/current.js
+++ src/controllers/auth/current.js
@@ -1,7 +1,7 @@
 const { asyncWrapper, responseData } = require('../../helpers/apiHelpers');
 
 const currentUser = async (req, res) => {
-  res.status(200).json(responseData(req.user, 200));
+  res.status(200).json(responseData({ user: req.user }, 200));
 };
 
 module.exports = asyncWrapper(currentUser);
